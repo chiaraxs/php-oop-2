6d39a373258dedf42ed05b600a8d611bcc62d73e
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-02-28 18:00:58 +0100

    Error in data

--- classes/food.php
+++ classes/food.php
@@ -17,13 +17,13 @@ class Food extends Products {
     // / variables
 
     // methods
-    public function __construct(string $_brand, string $_expirationDate, int $_weight, float $_price){
+    public function __construct(string $_productName, int $_productCode, string $_productType, string $_brand, string $_expirationDate, int $_weight, float $_price){
         parent :: __construct($_productName, $_productCode, $_productType);
 
-        $this -> brand = $_brand;
-        $this -> expirationDate = $_expirationDate;
-        $this -> weight = $_weight;
-        $this -> price = $_price;
+        $this->brand = $_brand;
+        $this->expirationDate = $_expirationDate;
+        $this->weight = $_weight;
+        $this->price = $_price;
     }
 
     /**
